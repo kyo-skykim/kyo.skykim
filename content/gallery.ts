16c8c0e401f60d0464d30b76b50492ba5b346f9b
kyo-skykim <aomsinzxc@gmail.com>
2026-05-23 21:03:19 +0700

    Add new photos to the gallery22052026

--- content/gallery.ts
+++ content/gallery.ts
@@ -8,4 +8,6 @@ export type Photo = {
 export const photos: Photo[] = [
   { src: "gallery/shabu_2_may_2026.jpg", caption: "I love shabuuuuu", date: "2026-05-02", location: "Park Chula" },
   { src: "gallery/giewnam_15_may_2026.jpg", caption: "เกี๊ยวน้ำอร่อยมาก ร้านประจำตั้งแต่เด็ก", date: "2026-05-15", location: "ซอยนารี" },
+  { src: "gallery/ori_22-05-2026.jpg", caption: "สาวสัตวแพทย์มขมาเจอที่ mrt ได้ำงงงง", date: "2026-05-22", location: "MRT บางหว้า" },
+  { src: "gallery/boon-22-05-2026.jpg", caption: "ละหมอวชิระเขามาคนเดียวไม่ได้ ชั้นเข้าเมืองก็เอาไปทรมานขาเล่น", date: "2026-05-22", location: "วชิรพยาบาล" },
 ];
